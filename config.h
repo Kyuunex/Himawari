47691ccb6a5f301901bf702a3e09685cfbc0d0cf
Kyuunex <kyuunex@protonmail.ch>
2024-10-22 19:29:28 +0400

    Initial upload

---
+++ config.h
@@ -0,0 +1,11 @@
+// #define USER_MODIFIER_KEY WLR_MODIFIER_ALT
+#define USER_MODIFIER_KEY WLR_MODIFIER_LOGO
+
+static const char *term_cmd = "kitty";
+static const char *runner_cmd = "wofi --show drun";
+static const char *fileman_cmd = "dolphin";
+
+static const char *startup[] = {
+    "/usr/bin/lxqt-policykit-agent",
+};
+
